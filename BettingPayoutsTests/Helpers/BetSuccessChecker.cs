4ccf123a233df742543c8f9f2b2a1b0894bdef65
dstaciwa <damian_staciwa@outlook.com>
2023-04-03 19:49:30 +0200

    Helpers added: - BetSuccessChecker.cs - OddsGenerator.cs

---
+++ BettingPayoutsTests/Helpers/BetSuccessChecker.cs
@@ -0,0 +1,30 @@
+﻿using BettingPayoutsTests.Base;
+
+namespace BettingPayoutsTests.Helpers
+{
+    public class BetSuccessChecker
+    {
+        public bool IsBetSuccessfull(Match match, Bet bet)
+        {
+            if (match.HomeTeamScore > match.AwayTeamScore && bet.BetType.Equals(BetType.HomeTeamWins))
+                return true;
+
+            else if (match.HomeTeamScore > match.AwayTeamScore && (bet.BetType.Equals(BetType.AwayTeamWins) || bet.BetType.Equals(BetType.Draw)))
+                return false;
+
+            else if (match.HomeTeamScore == match.AwayTeamScore && bet.BetType.Equals(BetType.Draw))
+                return true;
+
+            else if (match.HomeTeamScore == match.AwayTeamScore && (bet.BetType.Equals(BetType.AwayTeamWins) || bet.BetType.Equals(BetType.AwayTeamWins)))
+                return false;
+
+            else if (match.HomeTeamScore < match.AwayTeamScore && bet.BetType.Equals(BetType.AwayTeamWins))
+                return true;
+
+            else if (match.HomeTeamScore < match.AwayTeamScore && (bet.BetType.Equals(BetType.HomeTeamWins) || bet.BetType.Equals(BetType.Draw)))
+                return false;
+
+            return false;
+        }
+    }
+}
---
+++ BettingPayoutsTests/Helpers/OddsGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace BettingPayoutsTests.Helpers
+{
+    public class OddsGenerator
+    {
+        public static double GenerateRandomDouble()
+        {
+            var random = new Random();
+            double value = random.NextDouble() * 9.8 + 0.1;
+
+            return value;
+        }
+    }
+}
